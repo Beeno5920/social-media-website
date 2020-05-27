6d9a277901f534cc26687d68be8f8316c2643971
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-05-27 13:18:01 +1000

    initialize the frontend

--- socialMediaSite/settings.py
+++ socialMediaSite/settings.py
@@ -142,3 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Set backend port to 3000
+CORS_ORIGIN_WHITELIST = [
+         'http://localhost:3000'
+]
