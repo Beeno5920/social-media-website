d4c81b56ef6f9c028e31ed90d5d0e820f5dbdf9d
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-06-20 18:37:22 +1000

    added Layout components

---
+++ frontend/src/components/Layout/Index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Grid from "@material-ui/core/Grid";
+
+import Header from './Header';
+import PostList from "../Post/PostList";
+
+function Layout() {
+    return (
+        <div>
+            <div>
+            <Header />
+            </div>
+            <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+                <PostList />
+            </Grid>
+        </div>
+    );
+}
+
+export default Layout;
