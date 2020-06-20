00fe305a421a864625134934cc8b3d3a0d08e7fe
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-06-20 18:35:41 +1000

    updated the layout of the home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
-import PostList from '../components/Post/PostList'
+import Layout from "../components/Layout/Index";
 
 
 class HomePage extends Component {
 
     render() {
         return (
-            <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
-                <PostList />
-            </Grid>
+            <Layout />
         )
     }
 }
