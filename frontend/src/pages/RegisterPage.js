4a2ecc57d12959b723566fdc50c5d37c744988ac
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-06 18:23:34 +1000

    added register page

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {createMuiTheme, makeStyles} from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles"
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = (theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+});
+
+class RegisterPage extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const username = event.target[0].value;
+        const email = event.target[2].value;
+        const password = event.target[4].value;
+
+        if (!username || !email || !password) {
+            alert("Please enter username, email and password to sign up!");
+            return;
+        }
+
+        axios
+            .post("register/", {"email": email, "username": username, "password": password})
+            .then(res => {
+                axios
+                    .post("login/", {"email": email, "password": password})
+                    .then(res => this.props.setAuth(res.data.token, res.data.id))
+                    .catch(err => alert(err));
+            })
+            .catch(err => alert(err));
+
+        this.props.history.push('/');
+    }
+
+    render() {
+
+        const { classes } = this.props;
+
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="Username"
+                                    name="username"
+                                    autoComplete="username"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link href="/login" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        );
+    }
+}
+
+export default withStyles(useStyles)(RegisterPage);
